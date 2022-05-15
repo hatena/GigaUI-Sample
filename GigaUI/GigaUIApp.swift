82cca1180ed3842d8ce4f10bd75f5fc853425c2b
Kouki Saito <dan.addr.skd@gmail.com>
2022-05-15 14:20:09 +0900

    Initial Commit

---
+++ GigaUI/GigaUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  GigaUIApp.swift
+//  GigaUI
+//
+//  Created by Kouki Saito on 2022/05/15.
+//
+
+import SwiftUI
+
+@main
+struct GigaUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
